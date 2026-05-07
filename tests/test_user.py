379fee56e38867f01f704b6a6d759b9fa6907a98
Davletkildeeva <davletkildeevaaa@mail.ru>
2026-05-07 04:27:40 -0400

    Add tests and workflows

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,41 +1,33 @@
 from fastapi.testclient import TestClient
-
 from src.main import app
 
 client = TestClient(app)
 
-# Существующие пользователи
 users = [
-    {
-        'id': 1,
-        'name': 'Ivan Ivanov',
-        'email': 'i.i.ivanov@mail.com',
-    },
-    {
-        'id': 2,
-        'name': 'Petr Petrov',
-        'email': 'p.p.petrov@mail.com',
-    }
+    {'id': 1, 'name': 'Ivan Ivanov', 'email': 'i.i.ivanov@mail.com'},
+    {'id': 2, 'name': 'Petr Petrov', 'email': 'p.p.petrov@mail.com'}
 ]
 
 def test_get_existed_user():
-    '''Получение существующего пользователя'''
     response = client.get("/api/v1/user", params={'email': users[0]['email']})
     assert response.status_code == 200
     assert response.json() == users[0]
 
 def test_get_unexisted_user():
-    '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'nonexistent@mail.com'})
+    assert response.status_code == 404
+    assert response.json()['detail'] == 'User not found'
 
 def test_create_user_with_valid_email():
-    '''Создание пользователя с уникальной почтой'''
-    pass
+    response = client.post("/api/v1/user", json={'name': 'New User', 'email': 'new@mail.com'})
+    assert response.status_code == 201
+    assert isinstance(response.json(), int)
 
 def test_create_user_with_invalid_email():
-    '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    response = client.post("/api/v1/user", json={'name': 'Ivan Ivanov', 'email': 'i.i.ivanov@mail.com'})
+    assert response.status_code == 409
+    assert response.json()['detail'] == 'User with this email already exists'
 
 def test_delete_user():
-    '''Удаление пользователя'''
-    pass
+    response = client.delete("/api/v1/user", params={'email': 'p.p.petrov@mail.com'})
+    assert response.status_code == 204
